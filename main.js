eb7d70af49530ba52f62c9651408707ad10b9350
adao74 <andreaminghweidao@hotmail.com>
2024-07-20 22:42:09 -0500

    add DOM for useres

--- main.js
+++ main.js
@@ -10,8 +10,28 @@ const getUsers = () => {
     .then(res => res.json())
     .then(res => {
         //console.log(res); // get the response schema
-        users.push(res);
-        display(res.results)
+        // users.push(res); // not needed anymore b/c you are fetching multiple users at a time and they are returned in an array
+        displayNamePicture(res.results)
+    })
+}
+
+
+const displayNamePicture = (resultsArray) => {
+    console.log(resultsArray)
+    
+    resultsArray.forEach( (user, index) => {
+        
+        const newElement = document.createElement("p")
+        newElement.innerText = `User #${index + 1}: ${user.name.title} ${user.name.first} ${user.name.last}` 
+        const newButton = document.createElement("button")
+        newButton.innerText = "Click for more"
+        document.getElementById("users").appendChild(newElement)
+        document.getElementById("users").appendChild(newButton)
+        newButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            display(resultsArray)
+        });
+
     })
 }
 
@@ -32,13 +52,10 @@ const display = (resultsArray) => {
 const stringifyObjects = (newElement, user) => {
     
     const userKeys = Object.keys(user) // array of keys
-    
 
     console.log(userKeys)
 
     for (let i = 0; i < userKeys.length; i++) {
-
-
         if ( (typeof user[userKeys[i]] != "object") || (user[userKeys[i]] == null) || (user[userKeys[i]] == undefined) ) {
             console.log(`String key: ${userKeys[i]}: ${user[userKeys[i]]}`)
             newElement.innerText = `${newElement.innerText} \n ${userKeys[i]}: ${user[userKeys[i]]}`
@@ -46,14 +63,15 @@ const stringifyObjects = (newElement, user) => {
         } else {
             stringifyObjects(newElement, user[userKeys[i]])
         }
-
     }
-        // forEach instead of for loop
-        // userKeys.forEach( key => {
-        //     if (typeof user[key] === "string") { 
-        //         console.log(`String key: ${user[key]}`)
-        //         newElement.innerText = `${newElement.innerText} \n ${key}: ${user[key]}`
-        //         document.getElementById("users").appendChild(newElement)
-        //     }
-        // })  
+
+
+    // forEach instead of for loop
+    // userKeys.forEach( key => {
+    //     if (typeof user[key] === "string") { 
+    //         console.log(`String key: ${user[key]}`)
+    //         newElement.innerText = `${newElement.innerText} \n ${key}: ${user[key]}`
+    //         document.getElementById("users").appendChild(newElement)
+    //     }
+    // })  
 }
